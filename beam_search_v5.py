84db5e3014242eaec6440409fa6545d42e555458
beckydvn <18rldv@queensu.ca>
2022-12-12 15:12:48 -0500

    begin to move out new beam creation into a func

--- beam_search_v5.py
+++ beam_search_v5.py
@@ -1,6 +1,6 @@
 from cmath import log
 from dataclasses import dataclass
-from typing import List, Union
+from typing import List, Union, Dict
 from hovor.rollout.rollout_core import HovorRollout
 from hovor.rollout.graph_setup import BeamSearchGraphGenerator
 
@@ -45,16 +45,35 @@ class Beam:
     scores: list
 
 
+def _new_beam_from_output(beams: List[Beam], output: Dict):
+    at_beam = output.beam
+    if isinstance(output, Action):
+        last_action = output
+        last_intent = beams[at_beam].last_intent
+    else:
+        last_intent = output
+        last_action = beams[at_beam].last_action
+    return Beam(
+        at_beam,
+        last_action,
+        last_intent,
+        beams[at_beam].rankings + [output],
+        beams[at_beam].rollout.copy(),
+        beams[at_beam].scores + [log(output.probability)],
+    )
+
+
 def beam_search(k, max_fallbacks, conversation, output_files_path, filename):
     beams = []
     graph_gen = BeamSearchGraphGenerator(k)
     for i in range(len(conversation)):
         if i == 0:
             start_rollout = HovorRollout(output_files_path)
-            starting_values = start_rollout.get_action_confidences(
-                conversation[i]
-            )
-            outputs = [Action(key, val, index, log(val)) for index, (key, val) in enumerate(starting_values.items())]
+            starting_values = start_rollout.get_action_confidences(conversation[i])
+            outputs = [
+                Action(key, val, index, log(val))
+                for index, (key, val) in enumerate(starting_values.items())
+            ]
             # if there are less starting actions than there are beams, duplicate the best action until we reach k
             while len(outputs) < k:
                 outputs.append(outputs[0])
@@ -105,66 +124,74 @@ def beam_search(k, max_fallbacks, conversation, output_files_path, filename):
             )
         else:
             outputs = []
-            given_conv = [[] for _ in range(k)]
             for beam in range(k):
-                # call hovor to predict next for now using dummy function
                 if "USER" in conversation[i].keys():
-                    given_conv[beam] = beams[beam].rollout.get_highest_intents(
+                    all_intent_confs = beams[beam].rollout.get_highest_intents(
                         beams[beam].last_action.name, conversation[i]
                     )
-                    for val in range(len(given_conv[beam])):
-                        intent = given_conv[beam][val]["intent"]
-                        probs = given_conv[beam][val]["confidence"]
-                        outcome = given_conv[beam][val]["outcome"]
+                    for intent_cfg in all_intent_confs:
                         # find the score by taking the sum of the current beam thread which should be a list of log(prob)
-                        score = sum(beams[beam].scores) + log(probs)
-                        outputs.append(Intent(intent, probs, outcome, beam, score.real))
+                        outputs.append(
+                            Intent(
+                                intent_cfg["intent"],
+                                intent_cfg["confidence"],
+                                intent_cfg["outcome"],
+                                beam,
+                                (
+                                    sum(beams[beam].scores)
+                                    + log(intent_cfg["confidence"])
+                                ).real,
+                            )
+                        )
                 elif "HOVOR" in conversation[i].keys():
-                    given_conv[beam] = beams[
-                        beam
-                    ].rollout.get_action_confidences(
+                    all_act_confs = beams[beam].rollout.get_action_confidences(
                         conversation[i],
                         beams[beam].last_action.name,
                         beams[beam].last_intent.name,
                         beams[beam].last_intent.outcome,
                     )
-                    for index, (key, val) in enumerate(given_conv[beam].items()):
-                        score = sum(beams[beam].scores) + log(val)
-                        outputs.append(Action(key, val, beam, score.real))
+                    for act, conf in all_act_confs.items():
+                        outputs.append(
+                            Action(
+                                act,
+                                conf,
+                                beam,
+                                (sum(beams[beam].scores) + log(conf)).real,
+                            )
+                        )
             # sort the new list keeping track of each thread instead of the outputs
             outputs.sort()
             all_outputs = outputs
             outputs = outputs[0:k]
             if "USER" in conversation[i].keys():
-                beam_holders = [[] for _ in range(k)]
-                list_intents = []
-                list_actions = []
-                list_rollout = []
-                list_scores = [[] for _ in range(k)]
+                # beam_holders = [[] for _ in range(k)]
+                # list_intents = []
+                # list_actions = []
+                # list_rollout = []
+                # list_scores = [[] for _ in range(k)]
                 graph_beam_chosen_map = {idx: [] for idx in range(k)}
-                for seq in range(k):
-                    at_beam = outputs[seq].beam
+                # for idx in range(k):
+                #     at_beam = outputs[idx].beam
 
-                    # setting up the next intents to add to the beam
-                    next_intent = outputs[seq]
-                    list_intents.append(next_intent)
+                #     # setting up the next intents to add to the beam
+                #     next_intent = outputs[idx]
+                #     list_intents.append(next_intent)
 
-                    # carrying thrugh the proper last actions from previous beams
-                    last_action = beams[at_beam].last_action
-                    list_actions.append(last_action)
-                    last_rollout = beams[at_beam].rollout
-                    list_rollout.append(last_rollout)
+                #     # carrying thrugh the proper last actions from previous beams
+                #     list_actions.append(beams[at_beam].last_action)
+                #     list_rollout.append(beams[at_beam].rollout)
 
-                    # keeping track of the old beam scores all unadded
-                    list_scores[seq].extend(beams[at_beam].scores)
-                    list_scores[seq].append(log(next_intent.probability))
+                #     # keeping track of the old beam scores all unadded
+                #     list_scores[idx].extend(beams[at_beam].scores)
+                #     list_scores[idx].append(log(next_intent.probability))
 
-                    # setting the root of the current beam that will get additions
-                    current_beam = beams[at_beam].rankings
-                    beam_holders[seq].extend(current_beam)
-                    beam_holders[seq].append(next_intent)
+                #     # setting the root of the current beam that will get additions
+                #     beam_holders[idx].extend(beams[at_beam].rankings)
+                #     beam_holders[idx].append(next_intent)
 
-                    graph_beam_chosen_map[at_beam].append(next_intent.name)
+                #     graph_beam_chosen_map[at_beam].append(next_intent.name)
+                for output in outputs:
+                    graph_beam_chosen_map[output.beam].append(output.name)
 
                 for beam, chosen_intents in graph_beam_chosen_map.items():
                     graph_gen.create_nodes_highlight_k(
@@ -180,27 +207,29 @@ def beam_search(k, max_fallbacks, conversation, output_files_path, filename):
                         beam,
                         chosen_intents,
                     )
-                beams = []
+                # beams = []
                 graph_beams_copy = graph_gen.beams
                 graph_gen.beams = []
+                beams = [_new_beam_from_output(beams, output) for output in outputs]
                 for new_beam in range(k):
-                    beams.append(
-                        Beam(
-                            new_beam,
-                            list_actions[new_beam],
-                            list_intents[new_beam],
-                            beam_holders[new_beam],
-                            list_rollout[new_beam].copy(),
-                            list_scores[new_beam],
-                        )
-                    )
+                    # beams.append(
+                    #     Beam(
+                    #         new_beam,
+                    #         list_actions[new_beam],
+                    #         list_intents[new_beam],
+                    #         beam_holders[new_beam],
+                    #         list_rollout[new_beam].copy(),
+                    #         list_scores[new_beam],
+                    #     )
+                    # )
                     beams[new_beam].rollout.update_state(
                         beams[new_beam].last_action.name,
                         beams[new_beam].last_intent.outcome,
                     )
                     # also update the graph so they point to the right beams
                     # grab the old beam placement
-                    old_beam_placement = list_intents[new_beam].beam
+                    # old_beam_placement = list_intents[new_beam].beam
+                    old_beam_placement = beams[new_beam].last_intent.beam
                     graph_gen.beams.append(
                         BeamSearchGraphGenerator.GraphBeam(
                             {
@@ -218,24 +247,21 @@ def beam_search(k, max_fallbacks, conversation, output_files_path, filename):
                 list_rollout = []
                 list_scores = [[] for _ in range(k)]
                 graph_beam_chosen_map = {idx: [] for idx in range(k)}
-                for seq in range(k):
-                    at_beam = outputs[seq].beam
+                for idx in range(k):
+                    at_beam = outputs[idx].beam
                     # setting up the next actions to add to the beam
-                    next_action = outputs[seq]
+                    next_action = outputs[idx]
                     list_actions.append(next_action)
                     # carry through the last intents from the previous beams
-                    last_intent = beams[at_beam].last_intent
-                    list_intents.append(last_intent)
+                    list_intents.append(beams[at_beam].last_intent)
 
-                    last_rollout = beams[at_beam].rollout
-                    list_rollout.append(last_rollout)
+                    list_rollout.append(beams[at_beam].rollout)
 
-                    list_scores[seq].extend(beams[at_beam].scores)
-                    list_scores[seq].append(log(next_action.probability))
+                    list_scores[idx].extend(beams[at_beam].scores)
+                    list_scores[idx].append(log(next_action.probability))
 
-                    current_beam = beams[at_beam].rankings
-                    beam_holders[seq].extend(current_beam)
-                    beam_holders[seq].append(next_action)
+                    beam_holders[idx].extend(beams[at_beam].rankings)
+                    beam_holders[idx].append(next_action)
 
                     graph_beam_chosen_map[at_beam].append(next_action.name)
 
@@ -314,7 +340,9 @@ def beam_search(k, max_fallbacks, conversation, output_files_path, filename):
     for final in range(len(beams)):
         if beams[final].rollout.get_reached_goal():
             graph_gen.set_last_chosen(beams[final].rankings[-1].name, final)
-            graph_gen.complete_conversation(round(beams[final].rankings[-1].score.real, 4))
+            graph_gen.complete_conversation(
+                round(beams[final].rankings[-1].score.real, 4)
+            )
         head = "0"
         for elem in beams[final].rankings:
             tail = head
@@ -334,10 +362,10 @@ if __name__ == "__main__":
             "HOVOR": "What invited talk do you want to see on Day 1? You can learn about Factored Transition Systems or the applications of Multi-Agent Path Finding."
         },
         {"USER": "I want to see the talk on Factored Transition Systems."},
+        {"HOVOR": "And then? What after the invited talk?"},
         {
-            "HOVOR": "And then? What after the invited talk?"
+            "USER": "I want to learn more about classical planning and why applying heuristics is useful."
         },
-        {"USER": "I want to learn more about classical planning and why applying heuristics is useful."},
         {
             "HOVOR": "What session do you want to see in the afternoon? Your options are: Model-Based Reasoning, Learning for Scheduling Applications, Search, and Optimal Planning."
         },
@@ -352,7 +380,9 @@ if __name__ == "__main__":
         {
             "HOVOR": "What session do you want to see in the morning? The sessions available are on Planning Representations and Scheduling, Verification, RL, or Heuristics in Classical Planning."
         },
-        {"USER": "I want to learn more about classical planning and why applying heuristics is useful."},
+        {
+            "USER": "I want to learn more about classical planning and why applying heuristics is useful."
+        },
         {
             "HOVOR": "What session do you want to see in the afternoon? Your options are: Model-Based Reasoning, Learning for Scheduling Applications, Search, and Optimal Planning."
         },
@@ -361,6 +391,10 @@ if __name__ == "__main__":
     ]
     # NOTE: FOR HOVORROLLOUT: RUN RASA MODEL BEFORE RUNNING
     output_dir = "C:\\Users\\Rebecca\\Desktop\\plan4dial\\plan4dial\\local_data\\rollout_no_system_icaps_bot_mini\\output_files"
-    beam_search(3, 1, icaps_conversation_entity_drop, output_dir, "icaps_entity_drop_goal")
-    # beam_search(3, 2, icaps_conversation_entity_drop, output_dir, "icaps_entity_drop_no_goal")
-    # beam_search(3, 1, icaps_conversation_break_both, output_dir, "icaps_break_both")
+    beam_search(
+        3, 1, icaps_conversation_entity_drop, output_dir, "icaps_entity_drop_goal"
+    )
+    beam_search(
+        3, 2, icaps_conversation_entity_drop, output_dir, "icaps_entity_drop_no_goal"
+    )
+    beam_search(3, 1, icaps_conversation_break_both, output_dir, "icaps_break_both")
